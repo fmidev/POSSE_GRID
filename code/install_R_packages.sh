4187673d0f5b8549db2cbb9f65167e51c619edcf
lapsrut <lapsrut@fmi.fi>
2019-08-23 15:28:36 +0300

    still not usable

--- code/install_R_packages.sh
+++ code/install_R_packages.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 # install R packages in Rsrc
+# does not work correctly yet, so has to be done by hand
+
+# partial list of the packeges
+# install.packages(c('sp','ncdf4','spatstat','spatstat.utils','spatstat.data','polyclip'),
+#  lib=Sys.getenv("POSSE_R_LIBS"))
 
 # source ../POSSE_profile.sh
 
@@ -21,7 +26,9 @@ echo "Here we go"
 # this has to be done in certain order
 RLIBS=""
 
-for f in ./Rsrc/*.tar.gz ; do
-    echo R CMD INSTALL -l ${POSSE_R_LIBS} ${f}
+RLIBS="geogrid_3.5.2.tar.gz  Rgrib2_1.2.8_ml.tar.gz fastgrid_0.9.10.tar.gz  MOSfieldutils_0.4.0.tar.gz"
+
+for f in $RLIBS ; do
+    echo R CMD INSTALL -l ${POSSE_R_LIBS} ./Rsrc/${f}
 done
 
